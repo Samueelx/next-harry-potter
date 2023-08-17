f7b69d4ea2846412a75ceb74d576c89b711ef8af
Samueelx <gsammygitau@gmail.com>
2023-08-17 11:44:00 +0300

    configured next images and add support for nested declarations.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    swcMinify: true,
+    optimizeFonts: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "ik.imagekit.io",
+            },
+        ],
+        minimumCacheTTL: 15000000,
+    },
+}
+
+const path = require('path')
+ 
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
 
 module.exports = nextConfig
